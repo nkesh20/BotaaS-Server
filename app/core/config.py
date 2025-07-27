e03a929c55e0fb74d284d9ceed1e28f0dbfb32cb
nkesh20 <nkesh20@freeuni.edu.ge>
2025-07-27 19:53:37 +0400

    fix broadcast scheduling and move base and webhook urls to env file

--- app/core/config.py
+++ app/core/config.py
@@ -25,14 +25,14 @@ class Settings(BaseSettings):
 
     # Database postgres
     DATABASE_URL: str = "postgresql://postgres:postgres@localhost/botaas"
-    WEBHOOK_BASE_URL: str = "https://2b5926a373fb.ngrok-free.app"
-    BASE_URL: str = "https://2b5926a373fb.ngrok-free.app"  # For webhook setup
+    WEBHOOK_BASE_URL: str = os.getenv("WEBHOOK_BASE_URL", "localhost:8000")
+    BASE_URL: str = os.getenv("BASE_URL", "localhost:8000")
 
     # Security
     SECRET_KEY: str = os.getenv("SECRET_KEY", "development_secret_key")
     TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "development_bot_token")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
 
     class Config:
         case_sensitive = True
